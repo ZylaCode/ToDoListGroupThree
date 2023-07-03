78daa50be8bdf14b31aa7384f3a9e095f3a8a386
SanjaRogovic <sanja210@gmail.com>
2023-07-03 02:12:44 +0200

    created new html and js

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
-function displaydate() {
-    let date = new Date()
-    date = date.toString().split(" ")
-    document.querySelector("#date").innerHTML = date[1] + " " + date[2] + " " + date[3]
-}
+// function displaydate() {
+//     let date = new Date()
+//     date = date.toString().split(" ")
+//     document.querySelector("#date").innerHTML = date[1] + " " + date[2] + " " + date[3]
+// }
 
-window.onload = function(){
-    displaydate()
-}
-
-document.querySelector(".add").addEventListener("click", () => {
-    let item = document.querySelector("#myInput")
-    createItem(item)
-})
-
-function addItem() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let text = document.createTextNode(inputValue);
-
-    li.appendChild(text);
+// window.onload = function(){
+//     displaydate()
+// }
 
 
-    if (inputValue === '') {
-        alert('Please add an item on the list!');
+//// UPDATED JS
+
+const form = document.querySelector('#new-task-form');
+const input = document.querySelector('#new-task-input');
+const task = document.querySelector('#tasks');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (input.value.trim() === "") {
+        alert('Please add a task!')
     } else {
-        document.getElementById("myUL").appendChild(li);
-    }
+        // const task = input.value;
 
-    document.getElementById("myInput").value = "";
+        const paragraph= document.createElement('p')
+        paragraph.innerText = input.value;
+        paragraph.classList.add('p'); //this class connects to CSS for styling
+        task.appendChild(paragraph);
+        input.value = "";
+
+        paragraph.addEventListener('click', () => {
+            paragraph.style.textDecoration = 'line-through';
+        })
+
+
+        paragraph.addEventListener('dblclick', () => {
+            task.removeChild(paragraph);
+        })
+
+
+        // another option below but I don't know how to manage EDIT and DELETE buttons
+
+        // const buttonEdit = document.createElement('button')
+        // buttonEdit.classList.add('edit');
+        // buttonEdit.innerHTML = 'Edit';
+        // taskList.appendChild(buttonEdit);
+
+        // const buttonDelete = document.createElement('button')
+        // buttonDelete.classList.add('delete');
+        // buttonDelete.innerHTML = 'Delete';
+        // taskList.appendChild(buttonDelete);
+        // input.value = "";
+
+        // buttonEdit.addEventListener('click', () => {
+        //     if (buttonEdit.innerText === 'edit'){
+        //         paragraph.removeAttribute('input');
+        //     buttonEdit.innerText = 'Save';
+        //     } else {
+        //         return;
+        //     }          
+        // })
+
+        // buttonDelete.addEventListener('click', () => {
+        //     taskList.removeChild(paragraph);
+            
+        // })
+  
    
-    } 
-
-const checkbox = document.querySelectorAll('.checkbox');
-
-let unchecked = true;
-
-checkbox.forEach((checkbox)=>{
-checkbox.addEventListener("click", (event)=>{
-    if (unchecked){checkbox.src = "imgs/square-check-regular.svg";
-} else {
-    checkbox.src = "imgs/square-regular.svg"
-}
-unchecked = !unchecked
-});
+    };
 });
